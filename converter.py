15df3afd3cebb9d4ea3bffc7b48a0552d633a6da
Kozhinov Pavel <111296443+oAnthocyane@users.noreply.github.com>
2024-05-27 23:25:23 +0300

    Fix bag with variable name. Cleaned code

--- converter.py
+++ converter.py
@@ -1,15 +1,12 @@
 import os
 import sys
 
-
 CHARGE = sys.argv[1]
 MULTIPLICITY = sys.argv[2]
-
 
 def get_coord(file):
     inp = file.readlines()[2:]
     return "\n".join(inp)
-
 
 def writer(calc_file, inp_file):
     start_values = "".join(calc_file)
@@ -26,35 +23,32 @@ file_template = open(file_template_name, "r").readlines()
 list_dir = os.listdir()
 
 for el in list_dir:
-    if ".xyz" in el:
+    if el.endswith(".xyz"):
         name_molecule = el
         break
 else:
-    assert "Not have files"
-  
+    raise AssertionError("No .xyz files found")
+
 for name_molecule in list_dir:
     extension = name_molecule[-3:]
-    if extension != "xyz": continue
-    
+    if extension != "xyz":
+        continue
+
     file_name = name_molecule[:-4]
-        
-    xyz_file = open(name_molecule, "r")
-    ans = get_coord(xyz_file)
-    
+
+    with open(name_molecule, "r") as xyz_file:
+        ans = get_coord(xyz_file)
+
     inp_file_name = file_name + ".inp"
-    inp_file = open(inp_file_name, "w")
-    template = writer(file_template, ans)
-    inp_file.write(template)
+    with open(inp_file_name, "w") as inp_file:
+        template = writer(file_template, ans)
+        inp_file.write(template)
 
-    inp_file.close()
-    xyz_file.close()
-    
-    dir_name = os.path.dirname(os.path.abspath(name))
+    dir_name = os.path.dirname(os.path.abspath(file_name))
     os.mkdir(file_name)
-    first = dir_name + "\\" + name_molecule
-    second = dir_name + "\\" + file_name + "\\" + name_molecule
+    first = os.path.join(dir_name, name_molecule)
+    second = os.path.join(dir_name, file_name, name_molecule)
     os.replace(first, second)
-    first = dir_name + "\\" + inp_file_name
-    second = dir_name + "\\" + file_name + "\\" + inp_file_name
+    first = os.path.join(dir_name, inp_file_name)
+    second = os.path.join(dir_name, file_name, inp_file_name)
     os.replace(first, second)
-
